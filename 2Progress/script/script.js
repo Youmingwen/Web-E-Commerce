083c1500f64db6a8676afcc38ce467dbd500ea55
Youmingwen <owen.535180016@stu.untar.ac.id>
2021-02-13 22:15:33 +0700

    Project
    Halaman Shop dan ShopDetail Hampir Selesai serta menemukan adanya kesalahan pada desain navbar sehingga perlunya perbaikan

---
+++ 2Progress/script/script.js
@@ -0,0 +1,8 @@
+// Page ShopDetail Quantity
+function increment() {
+    document.getElementById('quantity').stepUp();
+}
+
+function decrement() {
+    document.getElementById('quantity').stepDown();
+}
