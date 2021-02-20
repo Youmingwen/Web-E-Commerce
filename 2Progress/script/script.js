be99c8f0538c48fed0928700d91b341f35e31186
Youmingwen <owen.535180016@stu.untar.ac.id>
2021-02-20 20:15:40 +0700

    Project
    Big Fix Index.html dan beberapa page lainnya

--- 2Progress/script/script.js
+++ 2Progress/script/script.js
@@ -6,3 +6,13 @@ function increment() {
 function decrement() {
     document.getElementById('quantity').stepDown();
 }
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
