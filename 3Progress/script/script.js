95b724d547c3c0d155e57e5c3cd54612337d7cde
Youmingwen <owen.535180016@stu.untar.ac.id>
2021-02-23 19:11:29 +0700

    Project
    Progress 3

---
+++ 3Progress/script/script.js
@@ -0,0 +1,21 @@
+// Page brandProduct Quantity
+function increment() {
+    document.getElementById('quantity').stepUp(1);
+}
+
+function decrement() {
+    document.getElementById('quantity').stepDown(1);
+}
+
+// Navbar
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+//Profile
